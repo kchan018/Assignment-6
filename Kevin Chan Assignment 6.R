29894a6a0a6a16789e3b66c02190a85ea8729ded
kchan018 <kchan018@bu.edu>
2018-03-26 21:04:02 -0400

    Updated to reflect changes in R Markdown and pdf.

--- Kevin Chan Assignment 6.R
+++ Kevin Chan Assignment 6.R
@@ -13,8 +13,12 @@ castle <- data.frame('lon' = -4.548768, 'lat' = 50.82903)
 # Pub
 barrel <- data.frame('lon' = -4.543023, 'lat' = 50.83007)
 
+## Hotels (Added by Steven)
+falcon <- data.frame('lon' = -4.547858, 'lat' = 50.82697)
+atlantic <- data.frame('lon' = --4.548378, 'lat' =  50.8314)
+
 # Bude Road Map
-budemap_road <- get_map('bude', zoom = 15)
+budemap_road <- get_map(bude + c(-0.005, 0.005), zoom = 15)
 ggmap(budemap_road) + 
   geom_point(aes(x = lon, y = lat), data = bude, color = "black", size = 2) + 
   geom_point(aes(x = lon, y = lat), data = summerleaze, color = "brown", size = 2) + 
@@ -22,10 +26,13 @@ ggmap(budemap_road) +
   geom_point(aes(x = lon, y = lat), data = cricket, color = "green", size = 2) + 
   geom_point(aes(x = lon, y = lat), data = pool, color = "purple", size = 2) + 
   geom_point(aes(x = lon, y = lat), data = castle, color = "gray", size = 2) + 
-  geom_point(aes(x = lon, y = lat), data = barrel, color = "yellow", size = 2)
+  geom_point(aes(x = lon, y = lat), data = barrel, color = "yellow", size = 2)+
+  # Hotels added 
+  geom_point(aes(x = lon, y = lat), data = falcon, color = "orange", size = 2)+
+  geom_point(aes(x = lon, y = lat), data = atlantic, color = "orange", size = 2)
 
 # Bude Watercolor Map
-budemap_watercolor <- get_map('bude', zoom = 15, maptype = 'watercolor')
+budemap_watercolor <- get_map(bude + c(-0.005, 0.005), zoom = 15, maptype = 'watercolor')
 ggmap(budemap_watercolor) + 
   geom_point(aes(x = lon, y = lat), data = bude, color = "black", size = 2) + 
   geom_point(aes(x = lon, y = lat), data = summerleaze, color = "brown", size = 2) + 
